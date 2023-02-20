f9de8a5be5e5712c1ca7cbf5414cad39e998b952
annabusaga <anna.busaga@gmail.com>
2023-02-20 15:53:32 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+
+const player = new Player(document.querySelector('#vimeo-player'));
+
+player.on('timeupdate', function ({ seconds }) {
+    localStorage.setItem("videoplayer-current-time", seconds)
+
+});
+
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time")|| 0);
+
+
+
+
+
+
+
 
